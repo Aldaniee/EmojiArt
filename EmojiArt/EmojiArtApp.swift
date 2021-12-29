eead3f5042fae35df2bc70f5f364a527f4dbf595
Aidan Lee <aidanwlee@gmail.com>
2021-12-29 11:07:55 -0500

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Aidan Lee on 12/29/21.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
